621b29a522b54bd65c4f7b5f8a03293c4eea0ebc
Arishenryl <95382595+Arishenryl@users.noreply.github.com>
2023-04-04 18:35:14 -0400

    Update bashportion.sh

--- bashportion.sh
+++ bashportion.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-
+fn=$1
+if [ "$fn" == '' ] || [ ${fn##*.} != 'pbd' ]
+then
+echo "please provide filename argument"
+else
+grep ^ATOM $fn > temp
+#echo $res
+awk -F ' ' '{print $7,$8,$9}' temp
+rm temp
+fi
